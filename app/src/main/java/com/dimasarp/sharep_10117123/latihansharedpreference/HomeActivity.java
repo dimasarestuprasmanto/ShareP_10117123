15502affb7f10149f34cdedb074060c21e1bc14d
dimasarestuprasmanto <dimas.arestu99@gmail.com>
2020-05-01 16:09:48 +0700

    Initial commit

---
+++ app/src/main/java/com/dimasarp/sharep_10117123/latihansharedpreference/HomeActivity.java
@@ -0,0 +1,41 @@
+package com.dimasarp.sharep_10117123.latihansharedpreference;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dimasarp.sharep_10117123.latihansharedpreferences.utils.Preferences;
+import com.dimasarp.sharep_10117123.R;
+
+public class HomeActivity extends AppCompatActivity {
+    private TextView txtKeluar;
+    private TextView txtName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        declareView();
+        txtKeluar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Clear Set Preferences
+                Preferences.setLogout(getBaseContext());
+
+                //Pindah Halaman ke Login
+                startActivity(new Intent(getBaseContext(), LoginActivity.class));
+                finish();
+            }
+        });
+    }
+    private void declareView() {
+        txtKeluar = findViewById(R.id.txt_logout);
+        txtName = findViewById(R.id.txtName);
+
+        txtName.setText(Preferences.getRegisteredUser(getBaseContext()));
+    }
+}
---
+++ app/src/main/java/com/dimasarp/sharep_10117123/latihansharedpreferences/utils/UtilStatic.java
@@ -0,0 +1,10 @@
+package com.dimasarp.sharep_10117123.latihansharedpreferences.utils;
+
+public interface UtilStatic {
+    // Default Value
+    String DEFAULT_STRING = "";
+    String STRING_0 = "0";
+    String STRING_NULL = "null";
+    int DEFAULT_INT = 0;
+    boolean DEFAULT_BOOLEAN = false;
+}
